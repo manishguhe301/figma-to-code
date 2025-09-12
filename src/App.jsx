f0764e5bb89f7c9824cc40b41f6562713b99646c
Manish <manishguhe301@gmail.com>
2025-09-12 18:34:12 +0530

    feat: complete navbar UI

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,11 @@ function App() {
     <div
       className="bg-gray-100 h-full w-full overflow-y-hidden"
     >
-      <div className="h-screen max-w-[425px] mx-auto overflow-y-scroll" id='mainDiv'>
+      <div
+        className="h-screen max-w-[425px] mx-auto overflow-y-scroll custom-scrollbar"
+        id='mainDiv'
+      >
         <NavBar />
-        <div className=""></div>
         <div className="pt-20 min-h-full rounded-t-4xl bg-white"></div>
       </div>
     </div >
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,17 +2,30 @@ import React from 'react'
 import battery from '../assets/battery.svg'
 import net from '../assets/net.svg'
 import Wifi from '../assets/Wifi.svg'
-
+import Arrow from '../assets/arrow-back.svg'
+import avatar from '../assets/avatar.svg'
 
 const NavBar = () => {
   const currentTime = new Date().toLocaleTimeString().slice(0, 4);
   return (
-    <div className='px-14 py-4 flex items-center justify-between w-full'>
-      <p className='text-lg font-semibold leading-6'>{currentTime}</p>
-      <div className="flex items-center gap-2 h-3 ">
-        {[net, Wifi, battery].map((src)=>{
-          return <img src={src} alt={src} key={src} className='h-full w-full'/>
-        })}
+    <div className='flex flex-col gap-2' >
+      <div className='px-14 py-4 flex items-center justify-between w-full'>
+        <p className='text-lg font-semibold leading-6'>{currentTime}</p>
+        <div className="flex items-center gap-2 h-3 ">
+          {[net, Wifi, battery].map((src) => {
+            return <img src={src} alt={src} key={src} className='h-full w-full' />
+          })}
+        </div>
+      </div>
+      <div className="flex items-center justify-between w-full px-4 pb-3">
+        <button className="flex items-center gap-1 cursor-pointer">
+          <img src={Arrow} alt="back" className='h-5 w-5' />
+          <p className='font-normal text-base'>Back</p>
+        </button>
+        <p className='font-semibold text-lg leading-6 tracking-tight'>
+          Dashboard
+        </p>
+        <img src={avatar} alt="avatar" />
       </div>
     </div>
   )
